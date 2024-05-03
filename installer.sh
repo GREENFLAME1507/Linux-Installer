334e82d5ac992391220e0651c91ebe44a3faea90
Greenflame <160883635+GREENFLAME1507@users.noreply.github.com>
2024-05-03 17:10:46 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,7 @@ BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 WHITE='\033[0;37m'
+PINK='\033[0;35m'
 NC='\033[0m'
 
 
@@ -24,7 +25,8 @@ echo"$logo"
 echo "What Linux distro do you want to install?:"
 echo "1. ${BLUE}Kali"
 echo "2. ${YELLOW}ubuntu"
-echo "3. ${RED}cancel installation!"
+echo "3. ${PINK}Debian!"
+echo "4. ${RED}cancel installation!"
 read -p "Enter your choice (1 or 2): " choice
 
 
@@ -34,16 +36,28 @@ case $choice in
        cd installers
        chmod +x kali-installer-lna.sh
        rm ubuntu-installer-lna.sh
+       rm debian-installer-lna.sh
       ./kali-installer-lna.sh
         ;;
     2)
       ${YELLOW} echo "executing chosen options"
        cd installers
        chmod +x ubuntu-installer-lna.sh
-       rm ubuntu-installer-lna.sh
+       rm kali-installer-lna.sh
+       rm debian-installer-lna.sh
       ./ubuntu-installer-lna.sh
         ;;
-    3)
+        
+     3)
+       ${YELLOW} echo "executing chosen options"
+        cd installers
+        chmod +x debian-installer-lna.sh
+        rm ubuntu-installer-lna.sh
+        rm kali-installer-lna.sh
+       ./debian-installer-lna.sh
+        ;;
+        
+    4)
     ${RED}   clear
        echo "ok cancelling install..."
        echo "install canceled"
